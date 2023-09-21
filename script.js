233a73808bbc8e544d48468e21949f68ac003c4b
Nicky <nickshah534@gmail.com>
2023-09-21 22:47:27 +1000

    Script added a bit

---
+++ script.js
@@ -0,0 +1,7 @@
+const johnSelectorBtn = document.querySelector('#john-selector')
+const janeSelectorBtn = document.querySelector('#jane-selector')
+const chatHeader = document.querySelector('.chat-header')
+const chatMessages = document.querySelector('.chat-messages')
+const chatInputForm = document.querySelector('.chat-input-form')
+const chatInput = document.querySelector('.chat-input')
+const clearChatBtn = document.querySelector('.clear-chat-button')
